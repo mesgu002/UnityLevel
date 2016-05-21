576240d31333513ae66b55ad480349ecb8214a87
U-THINKPAD\Montana <mesgu002@ucr.edu>
2016-05-21 16:07:01 -0700

    modified assests

---
+++ helloWorld01/Assets/followBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followBehaviour : MonoBehaviour {
+
+    GameObject player;
+    public float speed = 50f;
+	// Use this for initialization
+	void Start () {
+       player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.LookAt(player.transform);
+        this.transform.Translate(Vector3.forward*Time.deltaTime*speed);
+
+	}
+}
